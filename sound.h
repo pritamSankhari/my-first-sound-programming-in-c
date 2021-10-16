925e40413596aeba555622c51fb152992a9f367a
pritamSankhari <33205478+pritamSankhari@users.noreply.github.com>
2021-10-16 16:57:13 +0530

    Add files via upload

---
+++ sound.h
@@ -0,0 +1,230 @@
+#include<stdio.h>
+#include<windows.h>
+
+void C3(int t)
+{
+	Beep(130.813,t);
+} 	 	
+void Db3(int t)
+{
+ 	Beep(138.591,t); 
+}
+
+void D3(int t)
+{
+ 	Beep(146.832,t); 
+}
+
+void Eb3(int t)
+{
+ 	Beep(155.563,t); 
+}
+
+void E3(int t)
+{
+ 	Beep(164.814,t); 
+}
+
+void F3(int t)
+{
+ 	Beep(174.614,t); 
+}
+
+void Gb3(int t)
+{
+ 	Beep(184.997,t); 
+}
+
+void G3(int t)
+{
+ 	Beep(195.998,t); 
+}
+
+void Ab3(int t)
+{
+ 	Beep(207.652,t); 
+}
+
+void A3(int t)
+{
+ 	Beep(220,t); 
+}
+
+void Bb3(int t)
+{
+ 	Beep(233.082,t); 
+}
+
+void B3(int t)
+{
+ 	Beep(246.942,t); 
+}
+
+void C4(int t)
+{
+ 	Beep(261.626,t); 
+}
+
+void Db4(int t)
+{
+ 	Beep(277.183,t); 
+}
+
+void D4(int t)
+{
+ 	Beep(293.665,t); 
+}
+
+void Eb4(int t)
+{
+ 	Beep(311.127,t); 
+}
+
+void E4(int t)
+{
+ 	Beep(329.628,t); 
+}
+
+void F4(int t)
+{
+ 	Beep(349.228,t); 
+}
+
+void Gb4(int t)
+{
+ 	Beep(369.994,t); 
+}
+
+void G4(int t)
+{
+ 	Beep(391.995,t); 
+}
+
+void Ab4(int t)
+{
+ 	Beep(415.305,t); 
+}
+
+void A4(int t)
+{
+ 	Beep(440,t); 
+}
+
+void Bb4(int t)
+{
+ 	Beep(466.164,t); 
+}
+
+void B4(int t)
+{
+ 	Beep(493.883,t); 
+}
+
+void C5(int t)
+{
+ 	Beep(523.251,t); 
+}
+
+void Db5(int t)
+{
+ 	Beep(554.365,t); 
+}
+
+void D5(int t)
+{
+ 	Beep(587.33,t); 
+}
+
+void Eb5(int t)
+{
+ 	Beep(622.254,t); 
+}
+
+void E5(int t)
+{
+ 	Beep(659.255,t); 
+}
+
+void F5(int t)
+{
+ 	Beep(698.456,t); 
+}
+
+void Gb5(int t)
+{
+ 	Beep(739.989,t); 
+}
+
+void G5(int t)
+{
+ 	Beep(783.991,t); 
+}
+
+void Ab5(int t)
+{
+ 	Beep(830.609,t); 
+}
+
+void A5(int t)
+{
+ 	Beep(880,t); 
+}
+
+void Bb5(int t)
+{
+ 	Beep(932.328,t); 
+}
+
+void B5(int t)
+{
+ 	Beep(987.767,t); 
+}
+
+void C6(int t)
+{
+ 	Beep(1046.502,t); 
+}
+
+void Db6(int t)
+{
+ 	Beep(1108.731,t); 
+}
+
+void D6(int t)
+{
+ 	Beep(1174.659,t); 
+}
+
+void Eb6(int t)
+{
+ 	Beep(1244.508,t); 
+}
+
+void E6(int t)
+{
+ 	Beep(1318.51,t); 
+}
+
+void F6(int t)
+{
+ 	Beep(1396.913,t); 
+}
+
+void Gb6(int t)
+{
+ 	Beep(1479.978,t); 
+}
+
+void G6(int t)
+{
+ 	Beep(1567.982,t); 
+}
+/* 	 	  	 
+G# / Ab 	6 	1661.219 	0.205m 	 
+A 	6 	1760 	0.193m 	 
+A# / Bb 	6 	1864.655 	0.182m 	 
+B 	6 	1975.533 	0.172m 	 
+  	  	  	  	 
+C 	7 	2093.005
+*/
+
